8fc86305e129f08b2494637475189b270d49f261
frankjmontero <67277640+frankjmontero@users.noreply.github.com>
2021-01-13 13:44:10 -0400

    Adds missing classes from UML
    -Creates Person and Car clases.
    -Updates the methods and fields of the other classes and interface.

---
+++ TransportCompany/TransportCompany/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp
+{
+    class Car : Vehicle
+    {
+        internal override void StartpEngine()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void StopEngine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetWheels(int wheels)
+        {
+            this.wheels = wheels;
+        }
+        public int GetWheels()
+        {
+            return this.wheels;
+        }
+    }
--- TransportCompany/TransportCompany/Driver.cs
+++ TransportCompany/TransportCompany/Driver.cs
@@ -4,8 +4,37 @@ using System.Text;
 
 namespace ClientApp
 {
-	class Driver : IDriver
-	{
+    class Driver : IDriver
+    {
+        private String licenseId;
+        private Vehicle vehicle;
 
-	}
+        public void DriveVehicle()
+        {
+            Console.WriteLine("Driving...");
+        }
+
+        public void SetLicense(String licenseId)
+        {
+            this.licenseId = licenseId;
+        }
+        public String GetLicense()
+        {
+            return this.licenseId;
+        }
+
+        public void SetVehicle(Vehicle vehicle)
+        {
+            this.vehicle = vehicle;
+        }
+        public Vehicle GetVehicle()
+        {
+            return this.vehicle;
+        }
+
+        private bool CanDriveThisVehicle()
+        {
+            return true;
+        }
+    }
 }
