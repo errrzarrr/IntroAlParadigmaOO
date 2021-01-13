dac793c6d25e823943bcb8b1cb884ed90c56a6c1
David <raymondinf23@gmail.com>
2021-01-12 21:12:51 -0400

    Create Pilot class

---
+++ TransportCompany/TransportCompany/Pilot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp
+{
+
+    class Pilot : Person, IDriver
+    {
+
+        protected string license;
+
+        public void DriveVehicle()
+        {
+            Console.WriteLine("Hi! i'm your pilot drive");
+        }
+
+    }
+}
