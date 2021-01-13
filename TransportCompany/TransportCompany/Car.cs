8c4ae695627f95bcf16a3b9a583498a94fcbb983
David <raymondinf23@gmail.com>
2021-01-13 19:37:54 -0400

    Create Car class

---
+++ TransportCompany/TransportCompany/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp
+{
+    class Car : Vehicle
+    {
+
+        public Car(int wheels)
+        {
+            this.wheels = wheels;
+        }
+
+        internal override void StartEngine()
+        {
+            Console.WriteLine("TaxiDriver is starting Engine...");
+        }
+
+        internal override void StopEngine()
+        {
+            Console.WriteLine("TaxiDriver is stopping Engine...");
+        }
+
+        public int GetWheels()
+        {
+            return wheels;
+        }
+    }
+}
