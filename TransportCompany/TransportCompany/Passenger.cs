49f0a1c71d2919200cea0530e6e959aedbdfd377
frankjmontero <67277640+frankjmontero@users.noreply.github.com>
2021-01-14 21:47:30 -0400

    Adds new diagram
    The main purpose is to add a updated version of the Class Diagram

---
+++ TransportCompany/TransportCompany/Passenger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp
+{
+    class Passenger : Person
+    {
+        protected String origin;
+        protected String destiny;
+    }
+}
--- TransportCompany/TransportCompany/Person.cs
+++ TransportCompany/TransportCompany/Person.cs
@@ -6,30 +6,35 @@ namespace ClientApp
 {
     abstract class Person
     {
-        protected string name;
-        protected DateTime dateOfBirth;
+        protected string _name = "Nobody";
+        protected DateTime _dateOfBirth;
 
         public void SetName(String name)
         {
-            this.name = name;
+            if (int.TryParse(name, out _)) {
+                Console.WriteLine("Name cannot be numbers");
+            } else this._name = name;
         }
         public String GetName()
         {
-            return this.name;
+            return this._name;
         }
 
         public void SetDateOfBirth(DateTime dateOfBirth)
         {
-            this.dateOfBirth = dateOfBirth;
+            if (dateOfBirth <= DateTime.Today)
+            {
+                this._dateOfBirth = dateOfBirth;                
+            } else Console.WriteLine("Date of birth cannot be a future date");
         }
         public DateTime GetDateOfBirth()
         {
-            return this.dateOfBirth;
+            return this._dateOfBirth;
         }
 
         public void Talk()
         {
-            Console.WriteLine("A person talkin");
+            Console.WriteLine($"{this._name} is talking");
         }
 
 
