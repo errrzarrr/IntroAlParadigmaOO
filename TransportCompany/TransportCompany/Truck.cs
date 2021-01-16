be886e9b4ee8cdb0297b08a24dd213733ae013d6
David <raymondinf23@gmail.com>
2021-01-16 12:47:27 -0400

    Create Truck class

---
+++ TransportCompany/TransportCompany/Truck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp
+{
+    class Truck : Vehicle
+    {
+
+        public Truck(int wheels)
+        {
+            if (wheels == 8 || wheels == 10)
+                this.wheels = wheels;
+            else 
+                Console.WriteLine("A truck can only have 8 or 10 wheels.");
+        }
+
+        internal override void StartEngine()
+        {
+            Console.WriteLine("Truck engine is starting...");
+        }
+
+        internal override void StopEngine()
+        {
+            Console.WriteLine("Truck engine is stopping...");
+        }
+    }
+}
