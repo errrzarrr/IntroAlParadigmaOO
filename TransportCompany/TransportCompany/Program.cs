9d7b02cdf0499fb32fb8069b24a13020bc404cae
David <raymondinf23@gmail.com>
2021-01-16 12:48:27 -0400

    Add Main-Menu

--- TransportCompany/TransportCompany/Program.cs
+++ TransportCompany/TransportCompany/Program.cs
@@ -5,41 +5,108 @@ namespace ClientApp
 {
 	class Program
 	{
-		static void Main(string[] args) 
+
+		static readonly Car car1 = new Car();
+		static readonly Truck truck = new Truck(4);
+		static readonly Passenger passenger1 = new Passenger();
+		static readonly TaxiDriver taxiDriver = new TaxiDriver(DateTime.Parse("2004/6/1"), passenger1, car1, "1234");
+		static readonly Pilot pilot = new Pilot(DateTime.Parse("2008/6/1"), car1, "1234");
+		static readonly FreightDriver freight = new FreightDriver(DateTime.Parse("2004/6/1"), truck, "1234");
+
+		static void Main(string[] args)
 		{
 			RunConsoleApp();
 		}
 
 		private static void RunConsoleApp()
 		{
-			Console.WriteLine("Hello User!");
 
-			Car car1 = new Car(4);
-			Passenger passenger1 = new Passenger();
-			TaxiDriver taxiDriver = new TaxiDriver(passenger1, car1);
-			Pilot pilot = new Pilot();
+			Console.WriteLine("\nHello User!\n");
 
 			passenger1.SetName(GetNameOfUser());
 			Loading();
 			Console.Clear();
 
-			passenger1.SetOrigin(GetUserLocation());
-			Loading();
-			Console.Clear();
+			MainMenu(passenger1);
 
-			passenger1.SetDestiny(GetUserDestiny());
-			Loading();
-			Console.Clear();
+            Console.WriteLine("Thank you for using our services. Press any key to exit...");
+            Console.ReadKey();
+        }
 
+		private static void MainMenu(Passenger passenger)
+        {
+			int input = 0;
+			string[] array = {"1- Call a Taxi Driver.", "2- Call a Pilot Driver.", "3- Call a Freight Driver.", "4- Exit"};
 
-			taxiDriver.SetVehicle(car1);
-			taxiDriver.SetLicense("1234");
-			taxiDriver.DriveVehicle();
-			Console.WriteLine("Thank you for using our services. Press any key to exit...");
-			Console.ReadKey();
-		}
+			void RenderMenu(int input, string[] array)
+            {
+				Console.WriteLine("===================== MENU ======================\n");
 
-		private static void Loading()
+				foreach (string option in array)
+				{
+					Console.WriteLine(option);
+				}
+
+                input = int.Parse(Console.ReadLine());
+
+				Thread.Sleep(400);
+				Console.Clear();
+
+				switch (input)
+				{
+
+					case 1:
+
+						if (string.IsNullOrEmpty(taxiDriver.GetLicense()))
+							break;
+						else
+						{
+							passenger.SetOrigin(GetUserLocation());
+							Loading();
+							Console.Clear();
+
+							passenger.SetDestiny(GetUserDestiny());
+							Loading();
+							Console.Clear();
+
+							taxiDriver.DriveVehicle();
+
+							break;
+						}
+
+					case 2:
+
+						Console.WriteLine(input);
+
+						break;
+
+					case 3:
+
+						Console.WriteLine(input);
+
+						break;
+
+					case 4:
+
+						Console.WriteLine(input);
+
+						break;
+
+					default:
+
+						Console.WriteLine("You inserted an invalid option... Try again.");
+
+						RenderMenu(input, array);
+
+						break;
+
+				}
+			}
+
+            RenderMenu(input, array);
+        }
+
+        private static void Loading()
         {
 			int time = 300;
 			int timesOfExecution = 3;
@@ -52,21 +119,21 @@ namespace ClientApp
 			Console.WriteLine("");
 		}
 
-		public static string GetNameOfUser()
+		private static string GetNameOfUser()
         {
 			Console.WriteLine("What is your name?");
 			return Console.ReadLine();
 
 		}
 
-		public static string GetUserLocation()
+		private static string GetUserLocation()
 		{
 			Console.WriteLine("Where are you?");
 			return Console.ReadLine();
 
 		}
 
-		public static string GetUserDestiny()
+		private static string GetUserDestiny()
 		{
 			Console.WriteLine("What is your destiny?");
 			return Console.ReadLine();
