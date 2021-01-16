0042e5dcd8800c3da4aea4d86f6691d910a07ece
David <raymondinf23@gmail.com>
2021-01-16 12:46:44 -0400

    Create FreightDriver class

---
+++ TransportCompany/TransportCompany/FreightDriver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp
+{
+    class FreightDriver : Driver
+    {
+        public FreightDriver(DateTime dateOfBirth, Truck truck, string licenseId)
+        {
+            SetDateOfBirth(dateOfBirth);
+            if (DriverIsLegal(20)) SetLicense($"Heavy-{licenseId}");
+            else Console.WriteLine("FreightDriver can't have a license.");
+        }
+
+        public override void SetLicense(string licenseId)
+        {
+            SetLicense(licenseId);
+            
+        }
+    }
+}
