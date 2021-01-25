928556606aca75246965ac6f7088fe0f6d985f4b
frankjmontero <67277640+frankjmontero@users.noreply.github.com>
2021-01-24 23:44:55 -0400

    Update: TaxyDriver.cs | Deletes Program.cs
    -TxayDriver class no longer handles fee logic. Rather inheirtes it from his father.
    -Finetune wording of DrivePassenger.
    -Program.cs now belogn to assembly ClientAppMain in the solution.

--- TransportCompany/TransportCompany/TaxyDriver.cs
+++ TransportCompany/TransportCompany/TaxyDriver.cs
@@ -5,44 +5,24 @@ using System.Threading;
 
 namespace ClientApp
 {
-    class TaxyDriver : Driver
-    {        
-        private double _fee;
-
-        public TaxyDriver() { }
-        public TaxyDriver(Driver driver) : base(driver)
-        {
-            //
-        }
-
-        public void SetFee(double fee)
-        {
-            if (fee < 0)
-            {
-                Console.WriteLine("You must provide a positive number");
-            } else this._fee = fee;
-        }
-        public double GetFee()
-        {
-            return this._fee;
-        }
-        
+    public class TaxyDriver : Driver
+    {           
         public void DrivePassenger(Passenger passenger)
         {
-            Console.WriteLine($"Taxy driver {this._name} will drive {passenger.GetName()} from {passenger.GetOrigin()} to {passenger.GetDestination()}\n");
+            Console.WriteLine($"Taxy driver {this.name} will drive {passenger.GetName()} from {passenger.GetOrigin()} to {passenger.GetDestination()}\n");
             Thread.Sleep(1000);
 
-            this._vehicle.StartpEngine();
+            this.vehicle.StartEngine();
             Thread.Sleep(1000);
             this.DriveVehicle();
             Thread.Sleep(4000);
 
             Console.WriteLine($"Arrived at {passenger.GetDestination()}");
             Thread.Sleep(1000);
-            Console.WriteLine($"Passenger {passenger.GetName()} is going to be ${this._fee} please");
+            Console.WriteLine($"Passenger {passenger.GetName()} is going to be ${this.fee} please");
             Thread.Sleep(4000);
 
-            this._vehicle.StopEngine();
+            this.vehicle.StopEngine();
             Thread.Sleep(2000);
         }
     }
