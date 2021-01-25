d5bd8d4b3fbeb159b5f81f7703b960db27126661
frankjmontero <67277640+frankjmontero@users.noreply.github.com>
2021-01-24 23:31:30 -0400

    Update FreightDriver.cs
    Logic changes:
    -No customized constructor needed.
    -CanDriveThisVehicle now simple returns a true if instances of Vehicle is a Truck.
    -Personalize wording of SetLicense

--- TransportCompany/TransportCompany/FreightDriver.cs
+++ TransportCompany/TransportCompany/FreightDriver.cs
@@ -4,24 +4,30 @@ using System.Text;
 
 namespace ClientApp
 {
-    class FreightDriver : Driver
+    public class FreightDriver : Driver
     {
-        public FreightDriver() { }
-
-        public FreightDriver(Driver driver) : base(driver)
+        public override void SetLicense(string licenseId)
         {
-            //
-        }
-
-        public override void ObtainDriverLicense()
-        {
-            int age = DateTime.Today.Year - this.GetDateOfBirth().Year;
-
-            if (age < 20)
+            if (this.age < 20)
             {
-                Console.WriteLine("You are not old enough to get a truck driver's license");
+                Console.WriteLine("You are not old enough to get a Freight/Truck's driver license");
+            }            
+            else if ((licenseId == "") || (licenseId.Substring(0, 6) != "Heavy-"))
+            {
+                Console.WriteLine("This is not a valid Freight/Truck's license ID");
             }
-            else this._licenseId = $"Heavy-{this.GetName()}{this.GetDateOfBirth().Year}{this.GetDateOfBirth().Month}{this.GetDateOfBirth().Day}";
+            else this.licenseId = licenseId;
         }
+
+        protected override bool CanDriveThisVehicle()
+        {
+            return (vehicle is Truck) ? true : false;
+        }
+
+        public override string ObtainDriverLicense()
+        {
+            this.licenseId = $"Heavy-{this.GetName()}{this.GetDateOfBirth().Year}{this.GetDateOfBirth().Month}{this.GetDateOfBirth().Day}";
+            return this.licenseId;
+        }            
     }
 }
